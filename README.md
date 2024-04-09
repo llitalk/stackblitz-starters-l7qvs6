0c10394050acfa2b19ee98a18981ce10768202f0
llitalk <litalkaminsky@gmail.com>
2024-04-09 15:44:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-l7qvs6
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/llitalk/stackblitz-starters-l7qvs6)
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: ``,
+})
+export class App {}
+
+bootstrapApplication(App);
